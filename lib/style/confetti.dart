96d6dd7d91b1e2e0ede76782b7fd8b5d987a50c3
fuji <fdc.fuji@gmail.com>
2023-12-01 10:50:09 +0800

    word rocket

--- lib/style/confetti.dart
+++ lib/style/confetti.dart
@@ -46,23 +46,47 @@ class Confetti extends StatefulWidget {
 }
 
 class _ConfettiState extends State<Confetti> with TickerProviderStateMixin {
-  late final ConfettiController _controllerOnce = ConfettiController(
+
+  late final ConfettiController _controllerFireworks = ConfettiController(
       duration: const Duration(seconds: 1)
   );
   late final ConfettiController _controllerJet = ConfettiController(
       duration: const Duration(seconds: 1)
   );
 
-  Animation<AlignmentGeometry> animateVertically(double x) {
+  late final Animation<double> invaderLoopController = AnimationController(
+    duration: const Duration(seconds: 8),
+    vsync: this,
+  )..repeat(reverse: true);
+
+  Animation<AlignmentGeometry> animateInvaderLoop(double x) {
     return Tween<AlignmentGeometry>(
-      begin: Alignment(x, -1.0),
-      end: Alignment(x, 1.0),
-    ).animate(AnimationController(
-      duration: const Duration(seconds: 8),
+      begin: Alignment(x, -0.8),
+      end: Alignment(x, -0.2),
+    ).animate(invaderLoopController);
+  }
+
+  AnimationController buildRocketAnimationController() {
+    return AnimationController(
+      duration: const Duration(seconds: 2),
       vsync: this,
     )
-      ..repeat(reverse: true));
+      ..addListener(() {
+        setState(() {});
+      });
   }
+
+  late final leftRocketAnimationController = buildRocketAnimationController();
+  late final rightRocketAnimationController = buildRocketAnimationController();
+
+  Animation<AlignmentGeometry> animationWordRocket(double x, AnimationController controller) {
+    return Tween<AlignmentGeometry>(
+      begin: Alignment(x, 1.0),
+      end: Alignment(x, -2.0),
+    ).animate(controller);
+  }
+
+  static const xOffset = 0.5;
 
   late List<Path> charPaths;
 
@@ -76,20 +100,238 @@ class _ConfettiState extends State<Confetti> with TickerProviderStateMixin {
       // Parse the font
       var myFont = reader.parseTTFAsset(data);
       // Generate the complete path for a specific character
-      charPaths = [
-        myFont.generatePathForCharacter(97),
-        myFont.generatePathForCharacter(98),
-        myFont.generatePathForCharacter(99),
-      ];
-    });
+      // list of int from 97 to 122
 
-    _controllerJet.play();
+      charPaths = List.generate(6, (i) => 97 + i)
+          .map((i) => myFont.generatePathForCharacter(i))
+          .toList();
+    });
   }
 
   @override
   void dispose() {
-    _controllerOnce.dispose();
+    _controllerFireworks.dispose();
     super.dispose();
+  }
+
+  bool isLeftRocketVisible = false;
+  bool isRightRocketVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return SafeArea(
+      child: Stack(
+        children: <Widget>[
+          //CENTER -- Blast
+          AlignTransition(
+            alignment: animateInvaderLoop(-xOffset*1.2),
+            child: Stack(
+              children: [
+                buildFireworksWidget(),
+                _largeText( 'English', Colors.white ),
+              ],
+            ),
+          ),
+
+          //CENTER RIGHT -- Emit left
+          AlignTransition(
+            alignment: animateInvaderLoop(xOffset*1.2),
+            child: Stack(
+              children: [
+                buildFireworksWidget(),
+                _largeText( 'Teacher', Colors.white),
+              ],
+            ),
+          ),
+
+          // Bottom word stack
+          Align(
+            alignment: const Alignment(-(xOffset*1.3), 1.0),
+            child: Wrap(
+              children: [
+                Column(
+                  children: <Widget>[
+                    TextButton(
+                        onPressed: animateLeftRocket ,
+                        child: _textButton('ビサヤ語')),
+                    TextButton(
+                        onPressed: animateLeftRocket,
+                        child: _textButton('韓国語')),
+                    TextButton(
+                        onPressed: animateLeftRocket,
+                        child: _textButton('英語')),
+                    TextButton(
+                        onPressed: animateLeftRocket,
+                        child: _textButton('中国語')),
+                  ],
+                ),
+              ],
+            ),
+          ),
+
+          Align(
+            alignment: const Alignment(xOffset*1.3, 1.0),
+            child: Wrap(
+              children: [
+                Column(
+                  children: <Widget>[
+                    TextButton(
+                        onPressed: animateRightRocket,
+                        child: _textButton('先生')),
+                    TextButton(
+                        onPressed: animateRightRocket,
+                        child: _textButton('生徒')),
+                    TextButton(
+                        onPressed: animateRightRocket,
+                        child: _textButton('校長')),
+                    TextButton(
+                        onPressed: animateRightRocket,
+                        child: _textButton('学校')),
+                  ],
+                ),
+              ],
+            ),
+          ),
+
+          buildWordRocket('英語', -xOffset * 1.2, isLeftRocketVisible, leftRocketAnimationController) ,
+          buildWordRocket('校長', xOffset * 1.2, isRightRocketVisible, rightRocketAnimationController),
+
+        ],
+      ),
+    );
+  }
+
+  Visibility buildWordRocket(String text, double x, bool isVisible, AnimationController controller) {
+    return Visibility(
+        visible: isVisible,
+        child: AlignTransition(
+          alignment: animationWordRocket(x, controller),
+          child: Stack(alignment: Alignment.center, children: [
+            _largeText(text, Colors.yellow),
+            buildJetWidget(),
+          ]),
+        ));
+  }
+
+  ConfettiWidget buildFireworksWidget() {
+    return ConfettiWidget(
+      confettiController: _controllerFireworks,
+      blastDirectionality: BlastDirectionality.explosive,
+      maxBlastForce: 40,
+      emissionFrequency: 0.001,
+      shouldLoop: false,
+      // number of particles to emit
+      numberOfParticles: 80,
+      // start again as soon as the animation is finished
+      colors: const [
+        Color.fromARGB(255, 255, 255, 100),
+        Color.fromARGB(120, 255, 255, 100),
+      ],
+      createParticlePath: drawChar,
+      strokeWidth: 4,
+      strokeColor: const Color.fromARGB(80, 255, 255, 100),
+    );
+  }
+
+  ConfettiWidget buildJetWidget() {
+    return ConfettiWidget(
+      canvas: const Size(1500, 1500),
+      confettiController: _controllerJet,
+      blastDirectionality: BlastDirectionality.directional,
+      emissionFrequency: 0.50,
+      blastDirection: pi / 2,
+      gravity: 1.0,
+      shouldLoop: true,
+      numberOfParticles: 4,
+      // number of particles to emit
+      // start again as soon as the animation is finished
+      colors: const [
+        Colors.yellow
+      ],
+      createParticlePath: drawStar,
+      strokeWidth: 2,
+      strokeColor: const Color.fromARGB(80, 255, 255, 255),
+    );
+  }
+
+  Container _textButton(String text) {
+    return Container(
+      width: 200,
+      padding: const EdgeInsets.all(10),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.white,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: _text(text),
+    );
+  }
+
+  Container buildBorderContainer(Widget widget) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.white,
+        ),
+      ),
+      child: widget,
+    );
+  }
+
+  Text _largeText(String text, Color color) {
+    return Text(
+      text,
+      style: TextStyle(
+          color: color,
+          fontWeight: FontWeight.bold,
+          shadows: const [
+            Shadow(
+              blurRadius: 20.0,
+              color: Colors.yellow,
+              offset: Offset(5.0, 5.0),
+            ),
+          ],
+          fontSize: 50),
+    );
+  }
+
+  Text _text(String text) {
+    return Text(
+      text,
+      style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+          fontSize: 30),
+    );
+  }
+
+  animateLeftRocket() {
+    isLeftRocketVisible = true;
+    _controllerJet.play();
+    leftRocketAnimationController
+        .forward()
+        .timeout(const Duration(milliseconds: 1000), onTimeout: () {
+      _controllerFireworks.play();
+    }).then((value) {
+      leftRocketAnimationController.reset();
+      isLeftRocketVisible = false;
+    });
+  }
+
+  animateRightRocket() {
+    isRightRocketVisible = true;
+    _controllerJet.play();
+    rightRocketAnimationController
+        .forward()
+        .timeout(const Duration(milliseconds: 1000), onTimeout: () {
+      _controllerFireworks.play();
+    }).then((value) {
+      rightRocketAnimationController.reset();
+      isRightRocketVisible = false;
+    });
   }
 
   Path drawChar(Size size) {
@@ -122,186 +364,4 @@ class _ConfettiState extends State<Confetti> with TickerProviderStateMixin {
     return path;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    const xOffset = 0.5;
-    return SafeArea(
-      child: Stack(
-        children: <Widget>[
-          //CENTER -- Blast
-          AlignTransition(
-            alignment: animateVertically(-xOffset*1.2),
-            child: Stack(
-              children: [
-                buildFireworksWidget(),
-                _largeText( 'English', Colors.white ),
-              ],
-            ),
-          ),
-
-          //CENTER RIGHT -- Emit left
-          AlignTransition(
-            alignment: animateVertically(xOffset*1.2),
-            child: Stack(
-              children: [
-                buildFireworksWidget(),
-                _largeText( 'Teacher', Colors.white),
-              ],
-            ),
-          ),
-
-          Align(
-            alignment: const Alignment(-xOffset, 0.3),
-            child: Stack(
-              alignment: Alignment.center,
-                children: [
-              _largeText('英語', Colors.yellow),
-              buildJetWidget(),
-            ]),
-          ),
-
-          Align(
-            alignment: const Alignment(xOffset, 0.3),
-            child: Stack(
-                alignment: Alignment.center,
-                children: [
-              _largeText('校長', Colors.yellow),
-              buildJetWidget(),
-            ]),
-          ),
-
-          // Bottom word stack
-          Align(
-            alignment: const Alignment(-(xOffset*1.3), 1.0),
-            child: Wrap(
-              children: [
-                Column(
-                  children: <Widget>[
-                    TextButton(
-                        onPressed: () => _controllerOnce.play(),
-                        child: _textButton('ビサヤ語')),
-                    TextButton(
-                        onPressed: () => _controllerOnce.play(),
-                        child: _textButton('韓国語')),
-                    TextButton(
-                        onPressed: () => _controllerOnce.play(),
-                        child: _textButton('英語')),
-                    TextButton(
-                        onPressed: () => _controllerOnce.play(),
-                        child: _textButton('中国語')),
-                  ],
-                ),
-              ],
-            ),
-          ),
-
-          Align(
-            alignment: const Alignment(xOffset*1.3, 1.0),
-            child: Wrap(
-              children: [
-                Column(
-                  children: <Widget>[
-                    TextButton(
-                        onPressed: () => _controllerOnce.play(),
-                        child: _textButton('先生')),
-                    TextButton(
-                        onPressed: () => _controllerOnce.play(),
-                        child: _textButton('生徒')),
-                    TextButton(
-                        onPressed: () => _controllerOnce.play(),
-                        child: _textButton('校長')),
-                    TextButton(
-                        onPressed: () => _controllerOnce.play(),
-                        child: _textButton('学校')),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  ConfettiWidget buildFireworksWidget() {
-    return ConfettiWidget(
-      confettiController: _controllerOnce,
-      blastDirectionality: BlastDirectionality.explosive,
-      maxBlastForce: 40,
-      emissionFrequency: 0.001,
-      shouldLoop: false,
-      // number of particles to emit
-      numberOfParticles: 80,
-      // start again as soon as the animation is finished
-      colors: const [
-        Color.fromARGB(255, 255, 255, 100),
-        Color.fromARGB(120, 255, 255, 100),
-      ],
-      createParticlePath: drawChar,
-      strokeWidth: 4,
-      strokeColor: const Color.fromARGB(80, 255, 255, 100),
-    );
-  }
-
-  ConfettiWidget buildJetWidget() {
-    return ConfettiWidget(
-      confettiController: _controllerJet,
-      blastDirectionality: BlastDirectionality.directional,
-      emissionFrequency: 0.50,
-      blastDirection: pi / 2,
-      // don't specify a direction, blast randomly
-      shouldLoop: true,
-      numberOfParticles: 2,
-      // number of particles to emit
-      // start again as soon as the animation is finished
-      colors: const [
-        Colors.yellow
-      ],
-      createParticlePath: drawStar,
-      strokeWidth: 2,
-      strokeColor: const Color.fromARGB(80, 255, 255, 255),
-    );
-  }
-
-  Container _textButton(String text) {
-    return Container(
-      width: 200,
-      padding: const EdgeInsets.all(10),
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.white,
-        ),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: _text(text),
-    );
-  }
-
-  Text _largeText(String text, Color color) {
-    return Text(
-      text,
-      style: TextStyle(
-          color: color,
-          fontWeight: FontWeight.bold,
-          shadows: const [
-            Shadow(
-              blurRadius: 20.0,
-              color: Colors.yellow,
-              offset: Offset(5.0, 5.0),
-            ),
-          ],
-          fontSize: 50),
-    );
-  }
-
-  Text _text(String text) {
-    return Text(
-      text,
-      style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-          fontSize: 30),
-    );
-  }
 }
